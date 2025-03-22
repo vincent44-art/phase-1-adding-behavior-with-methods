c3f7cf21ee70b4d1a3f8c8d5150660cc98f3dd73
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-22 11:52:04 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+// Cat Class
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Dog Class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Bird Class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === "male") {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example Instances
+  const myCat = new Cat("Whiskers", "female");
+  console.log(myCat.speak()); // "Whiskers says meow!"
+  
+  const myDog = new Dog("Buddy", "male");
+  console.log(myDog.speak()); // "Buddy says woof!"
+  
+  const maleBird = new Bird("Charlie", "male");
+  console.log(maleBird.speak()); // "It's me! Charlie, the parrot!"
+  
+  const femaleBird = new Bird("Polly", "female");
+  console.log(femaleBird.speak()); // "Polly says squawk!"
+  
